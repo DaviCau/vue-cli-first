b8783b779e2863d2a0f1137fd59ef9b3d46132b4
DaviCau <davide.cau.rn@gmail.com>
2021-05-28 17:20:02 +0200

    Aggiunto titolo

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Title />
+    <!-- <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Title from './components/Title.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
+    Title
   }
 }
 </script>
@@ -19,10 +22,7 @@ export default {
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
 }
 </style>
---
+++ src/components/Title.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>La mia prima app con Vue CLI&excl;</h1>
+</template>
+
+<script>
+export default {
+  name: 'Title'
+}
+</script>
+
+<style>
+  h1 {
+      color: red;
+      font-size: 40px;
+  }
+</style>
